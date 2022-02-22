68c151539b15b1ddeee397d53ef96c55a7966fac
mm1ne <m.oborin@fingineers.ru>
2022-02-22 14:13:57 +0300

    ss

--- src/components/Counter/Counter.test.tsx
+++ src/components/Counter/Counter.test.tsx
@@ -4,11 +4,12 @@ import '@testing-library/jest-dom';
 import {counterButtonList, setCounterButtonsList} from '../../App';
 import userEvent from "@testing-library/user-event";
 import {Counter} from "./index";
+import {BrowserRouter} from 'react-router-dom';
 
 
 describe("<Counter />", () => {
     it('renders counterButtons', () => {
-        render(<Counter/>);
+        render(<BrowserRouter><Counter/></BrowserRouter>);
         expect(screen.getByTestId("incCounter by 1")).toBeInTheDocument()
 
         counterButtonList.map(item => {
@@ -20,21 +21,21 @@ describe("<Counter />", () => {
     });
 
     it('check incCounterByOne', () => {
-        render(<Counter/>);
+        render(<BrowserRouter><Counter/></BrowserRouter>);
         expect(screen.getByText(/Кликов по кнопкам: 0/i)).toBeInTheDocument()
         screen.getByTestId("incCounter by 1").click()
         expect(screen.getByText(/Кликов по кнопкам: 1/i)).toBeInTheDocument()
     });
 
     it('check setCounterByButtonValue', () => {
-        render(<Counter/>);
+        render(<BrowserRouter><Counter/></BrowserRouter>);
         setCounterButtonsList.map(item => {
             userEvent.click(screen.getByTestId(`setCounter by ${item.value}`))
             expect(screen.getByText(`Кликов по кнопкам: ${item.value}`)).toBeInTheDocument()
         })
     });
     it('check status active after click on button incCounter', () => {
-        render(<Counter/>);
+        render(<BrowserRouter><Counter/></BrowserRouter>);
         counterButtonList.map(item => {
             const button = screen.getByTestId(`incCounter by ${item.value}`)
             userEvent.click(button)
@@ -42,7 +43,7 @@ describe("<Counter />", () => {
         })
     });
     it('check status !active after click on another button incCounter', () => {
-        render(<Counter/>);
+        render(<BrowserRouter><Counter/></BrowserRouter>);
 
         const first_button = screen.getByTestId(`incCounter by ${counterButtonList[0].value}`)
         const second_button = screen.getByTestId(`incCounter by ${counterButtonList[1].value}`)
